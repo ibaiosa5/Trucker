a3f1a74fa8e50341b3507b49a6f3bb1ba65ccbf2
Ander Idoiaga <anderidoyaga@gmail.com>
2014-03-25 18:48:03 +0100

    Ultimos cambios

--- js/app/ruta.js
+++ js/app/ruta.js
@@ -7,7 +7,7 @@ APP.Ruta = (function(){
 
     //$('#comprobar').on('click', function(){
         //var login = $login.val();
-    var getRuta = function(){
+    /*var getRuta = function(){
         $.ajax({
                 url : 'data/ruta.json',
                 cache : false,
@@ -19,7 +19,20 @@ APP.Ruta = (function(){
                     console.log(errorThrown);
                 }
         });
-    };
+    };*/
+    var getRuta = function(){
+    $.ajax({
+      url: 'data/recorrido.kml',
+      type : 'GET',
+      dataType : 'xml',
+      success: function(data,textStatus, jqXHR) {
+        var $ruta = $(data);
+        recorrido=$ruta.find('coordinates').text();
+        console.log(recorrido);
+      }
+
+    });
+  };
 
     return{
         getRuta : getRuta
