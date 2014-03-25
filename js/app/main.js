90d77dc022a8d4f91ad77c2beaff4981bf67c366
DAW <daw@daw-VirtualBox.(none)>
2014-03-25 18:56:30 +0100

    listeners

--- js/app/main.js
+++ js/app/main.js
@@ -1,2 +1,37 @@
-var elDiv = document.getElementById("div_principal");
-elDiv.addEventListener("click", muestraMensaje, false);
+var APP = APP||{};
+APP.Controlador = (function(){
+    "use strict";
+
+    var muestraRuta = function(){
+        console.log("Pintar ruta");
+    };
+    var muestraCamion = function(){
+        console.log("Pintar ruta");
+    };
+    var muestraRemolque = function(){
+        console.log("Pintar ruta");
+    };
+
+
+
+        var laRuta = document.getElementById("ruta");
+        laRuta.addEventListener("click", muestraRuta, false);
+        console.log(laRuta);
+
+          var elCamion = document.getElementById("camion");
+        elCamion.addEventListener("click", muestraCamion, false);
+          var elRemolque = document.getElementById("remolque");
+        elRemolque.addEventListener("click", muestraRemolque, false);
+
+
+
+
+
+
+    return{
+
+    };
+
+})();
+
+APP.Controlador();
