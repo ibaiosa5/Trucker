7252b7ad9eefbb4dcd0a53df67166f0dfd0cbc75
DAW <daw@daw-VirtualBox.(none)>
2014-03-26 16:54:50 +0100

    pintar remolque camion

--- js/app/main.js
+++ js/app/main.js
@@ -2,6 +2,19 @@ $(function(){
     "use strict";
 
 
+
+    var mostrarAlertas = setInterval( function(e){
+
+        APP.Alertas.hayAlertas(function(alertas, textStatus, jqXHR){
+
+            if (alertas.alertas.length>0){
+
+                APP.UI.avisoAlertas(alertas.alertas);
+
+            }
+        }, 15000000);
+    });
+
     $(document).on('click', '#alertas', function(e){
         APP.Alertas.getAlertas(function(alertas, textStatus, jqXHR){
             if (alertas.alertas.length>0){
