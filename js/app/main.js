0649dbe9460b931616466f4213db0eebbc2e487b
DAW <daw@daw-VirtualBox.(none)>
2014-03-26 15:44:12 +0100

    dfd

--- js/app/main.js
+++ js/app/main.js
@@ -3,27 +3,47 @@ APP.Controlador = (function(){
     "use strict";
 
 
-$(document).on('click', $ruta, function(e){
-            console.log("han hecho click en alertas");
+        var laRuta  = $(document).on('click', '#ruta', function(e){
+            console.log("han hecho click en Ruta");
+        var ruta = APP.Ruta.getRuta();
+        //if (alertas.length){
+            console.log("hay ruta");
+            APP.UI.mostrarRuta();
+            console.log(ruta);
+        //} else APP.UI.esconderAlertas(alertas);
+        });
 
-        var alertas = APP.Alertas.getAlertas("00001");
-        if (alertas.length){
-            console.log("hay alertas");
-            APP.UI.mostrarAlertas(alertas);
-            console.log(alertas);
-        } else APP.UI.esconderAlertas(alertas);
-    });
+        var elCamion  = $(document).on('click', '#camion', function(e){
+            console.log("han hecho click en Camion");
+        var camion = APP.Camion.getCamion();
+        //if (alertas.length){
+            console.log("hay camion");
+            APP.UI.mostrarCamion();
+            console.log(camion);
+        //} else APP.UI.esconderAlertas(alertas);
+        });
+
+        var $remolque = $('#remolque');
+        var elRemolque  = $(document).on('click', $remolque, function(e){
+            console.log("han hecho click en Remolque");
+        var remolque = APP.Remolque.getRemolque();
+        //if (alertas.length){
+            console.log("hay remolque");
+            APP.UI.mostrarRemolque();
+            console.log($remolque);
+        //} else APP.UI.esconderAlertas(alertas);
+        });
 
 
-        var laRuta = document.getElementById("ruta");
-        laRuta.addEventListener("click", APP.Ui.muestraRuta(), false);
-        console.log(laRuta);
+        //var laRuta = document.getElementById("ruta");
+        //laRuta.addEventListener("click", APP.Ui.muestraRuta(), false);
+        //console.log(laRuta);
 
-          var elCamion = document.getElementById("camion");
-        elCamion.addEventListener("click", muestraCamion, false);
+        /*  var elCamion = document.getElementById("camion");
+        elCamion.addEventListener("click", APP.Camion.mostrarCamion, false);
           var elRemolque = document.getElementById("remolque");
-        elRemolque.addEventListener("click", muestraRemolque, false);
-
+        elRemolque.addEventListener("click", APP.remolque.mostrarRemolque, false);
+*/
 
 
 
--- js/app/ui.js
+++ js/app/ui.js
@@ -11,13 +11,13 @@ APP.UI = (function(lng, undefined) {
 
     };
 
-       var muestraRuta = function(){
+       var mostrarRuta = function(){
         console.log("Pintar ruta");
     };
-    var muestraCamion = function(){
+    var mostrarCamion = function(){
         console.log("Pintar camion");
     };
-    var muestraRemolque = function(){
+    var mostrarRemolque = function(){
         console.log("Pintar remolque");
     };
 
@@ -39,7 +39,9 @@ APP.UI = (function(lng, undefined) {
     return {
         mostrarAlertas : mostrarAlertas,
         esconderAlertas : esconderAlertas,
-        muestraRuta : muestraRuta
+        mostrarRuta : mostrarRuta,
+        mostrarCamion : mostrarCamion,
+        mostrarRemolque : mostrarRemolque
     };
 
 })();
